2e6babb9be3eb31084d539a0ac552dbe7ac8c96c
saisudhalahori <131282718+saisudhalahori@users.noreply.github.com>
2024-04-04 17:28:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
-    <AdminRout />
+{/*     <AdminRout /> */}
     </Provider>
     
     
